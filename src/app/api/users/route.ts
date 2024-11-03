17e73b0d33f710f4a030c8b962a9a25d3b758b0a
alim <wajihafatima689@gmail.com>
2024-11-03 23:46:06 +0500

    new change

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,41 +2,28 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDb from "../../../lib/connectDb";
 import userModel from "../../../model/userModel";
 
-// Ensure the database is connected once
-connectDb();
-
 export async function GET(req: NextRequest) {
-  const users = await userModel.find(); // Fetch all users from the database
-  return NextResponse.json(
-    {
-      isSuccessful: true,
-      users,
-    },
-    { status: 200 }
-  );
+  await connectDb();
+  const users = await userModel.find();
+  return NextResponse.json({ isSuccessful: true, users }, { status: 200 });
 }
 
 export async function POST(req: NextRequest) {
+  await connectDb();
   const { firstname, lastname, email, password } = await req.json();
   const newUser = new userModel({ firstname, lastname, email, password });
 
   try {
     await newUser.save();
     return NextResponse.json(
-      {
-        isSuccessful: true,
-        data: newUser, // Return the created user
-      },
-      { status: 201 } // Status code 201 for created resource
+      { isSuccessful: true, data: newUser },
+      { status: 201 }
     );
   } catch (error) {
     console.error("Error saving user:", error);
     return NextResponse.json(
-      {
-        isSuccessful: false,
-        message: "Error saving user",
-      },
-      { status: 500 } // Internal Server Error
+      { isSuccessful: false, message: "Error saving user" },
+      { status: 500 }
     );
   }
 }
