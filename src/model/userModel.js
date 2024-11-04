eea9eda914d2456197cc0d2bd3a4741b8c9238aa
alim <wajihafatima689@gmail.com>
2024-11-04 20:44:09 +0500

    .

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,29 +1,12 @@
 // /models/User.js
-import mongoose from "mongoose";
-const userSchema = new mongoose.Schema(
-  {
-    firstname: {
-      type: String,
-      required: true,
-    },
-    lastname: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+import mongoose from 'mongoose';
 
-// Create the User model
-const userModel = mongoose.models.User || mongoose.model("User", userSchema);
+const userSchema = new mongoose.Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
+const userModel = mongoose.model('users', userSchema);
 export default userModel;
