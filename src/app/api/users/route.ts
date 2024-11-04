bf553aa012cd7e8c2f59ef1b6fc3ee02dc9b6047
alim <wajihafatima689@gmail.com>
2024-11-04 14:53:39 +0500

    .

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,34 +2,30 @@ import { NextRequest, NextResponse } from "next/server";
 import {connectDb} from "../../../lib/connectDb";
 import userModel from "../../../model/userModel";
 
-export async function GET(req:NextRequest) {
+export async function GET(req: NextRequest) {
   try {
-    await connectDb(); // Ensure the database connection is established
-    const users = userModel.find({});
-    return NextResponse.json({ isSuccessful: true, users }, { status: 200 });
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    return NextResponse.json(
-      { isSuccessful: false, message: "Error fetching users" },
-      { status: 500 }
-    );
+      await connectDb(); 
+      const users = await userModel.find();
+      return NextResponse.json({ isSuccessful: true, users }, { status: 200 });
+  } catch (error: any) {
+      console.error("Error fetching users:", error.message);
+      return NextResponse.json(
+          { isSuccessful: false, message: "Error fetching users" },
+          { status: 500 }
+      );
   }
 }
 
 export async function POST(req:NextRequest) {
   try {
-    await connectDb(); // Ensure the database connection is established
-    const { firstname, lastname, email, password } = await req.json();
-
-    // Basic validation check
+    await connectDb(); 
+    const { firstname, lastname, email, password }: { firstname: string; lastname: string; email: string; password: string; } = await req.json();
     if (!firstname || !lastname || !email || !password) {
       return NextResponse.json(
         { isSuccessful: false, message: "All fields are required" },
         { status: 400 }
       );
     }
-
-    // Check if email already exists in the database
     const existingUser = await userModel.findOne({ email });
     if (existingUser) {
       return NextResponse.json(
@@ -37,11 +33,7 @@ export async function POST(req:NextRequest) {
         { status: 400 }
       );
     }
-
-    // Create a new user instance
     const newUser = new userModel({ firstname, lastname, email, password });
-
-    // Save the new user to the database
     await newUser.save();
     return NextResponse.json(
       { isSuccessful: true, data: newUser },
