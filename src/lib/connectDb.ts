1899704e78ee32951276d08c1a667ba7e120db1c
alim <wajihafatima689@gmail.com>
2024-11-04 20:08:28 +0500

    .

--- src/lib/connectDb.ts
+++ src/lib/connectDb.ts
@@ -1,40 +1,10 @@
-// import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-import mongoose from "mongoose";
-
-// let isConnected = false;
-
-// function connectDb() {
-//   if (isConnected) {
-//     console.log("Using existing database connection");
-//     return;
-//   }
-
-//   if (!process.env.MONGO_URI) {
-//     console.error("MONGO_URI is not defined");
-//     throw new Error("Database URI is missing");
-//   }
-
-//   try {
-//     console.log("Attempting to connect to MongoDB...");
-//      mongoose.connect(process.env.MONGO_URI, {
-//       serverSelectionTimeoutMS: 5000,
-//     });
-//     isConnected = true;
-//     console.log("Connected to database");
-//   } catch (error) {
-//     console.error("Database connection error:", error);
-//     throw new Error("Could not connect to the database");
-//   }
-// }
-// export default connectDb;
-// lib/connectDb.ts
-
-const MONGO_URI = process.env.MONGO_URI as string;
-
-if (!MONGO_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your Mongo URI to .env.local');
 }
+
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached:any = global.mongoose;
 
@@ -48,11 +18,16 @@ async function connectDb() {
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => mongoose);
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
   }
   cached.conn = await cached.promise;
   return cached.conn;
 }
 
 export default connectDb;
-
